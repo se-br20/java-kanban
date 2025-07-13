811c4f7b4e7106dc93a037f64a485280602f7c0d
se-br20 <125740942+se-br20@users.noreply.github.com>
2025-07-13 20:01:13 +0300

    Финальный проект спринта 8 с исправлением

--- src/task/Subtask.java
+++ src/task/Subtask.java
@@ -6,7 +6,7 @@ import java.time.LocalDateTime;
 public class Subtask extends Task {
     private final int epicId;
 
-    public Subtask(String name, String description, LocalDateTime startTime, Duration duration, int epicId ) {
+    public Subtask(String name, String description, LocalDateTime startTime, Duration duration, int epicId) {
         super(name, description, startTime, duration);
         this.epicId = epicId;
     }
